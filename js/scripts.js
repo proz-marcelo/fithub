c90bc574c8cd0ac2a5be02bf891516b941cf3468
proz-marcelo <proz.marcelo@protonmail.com>
2024-10-03 22:06:48 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,3 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
-// Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
-})
